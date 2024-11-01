facc55fb36af45c5b5edde83bd3c1d633fb20b8a
carlosgovi <carlosgovi.dev@gmail.com>
2024-11-01 09:41:33 -0300

    optimize img config nextJS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { unoptimized: true },
+const nextConfig = {images: {
+  formats: ['image/avif', 'image/webp'], // Habilita formatos de alta eficiencia
+  deviceSizes: [640, 750, 828, 1080, 1200, 1920], // Ajusta a los anchos de tus dispositivos principales
+  imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], // Dimensiones optimizadas según tus necesidades
+},
 };
 
-module.exports = nextConfig;
